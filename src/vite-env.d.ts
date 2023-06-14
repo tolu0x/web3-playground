71153cda791e7d4f0c43e17437adfe8bf82df907
tolu <theonlytolu@outlook.com>
2023-06-14 13:01:02 +0100

    updated ethereum window type definition

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,9 @@
 /// <reference types="vite/client" />
-import { ExternalProvider } from "@ethersproject/providers";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
 
 declare global {
     interface Window{
-        ethereum?: ExternalProvider;
+        ethereum?: MetaMaskInpageProvider;
     }
 }
